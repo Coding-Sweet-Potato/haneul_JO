9af81045f7f8c2680a1ba6bc9cfa294680b160e8
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-02-13 23:51:49 +0900

    Update Top Competitors.sql

--- week1/Top Competitors.sql
+++ week1/Top Competitors.sql
@@ -1,9 +1,9 @@
-SELECT H.hacker_id, H.name
-FROM Submissions as S
- INNER JOIN Challenges as C ON S.challenge_id = C.challenge_id
- INNER JOIN Difficulty as D ON C.difficulty_level = D.difficulty_level
- INNER JOIN Hackers as H ON S.hacker_id = H.hacker_id
-WHERE S.score = D.score AND C.difficulty_level = D.difficulty_level
-GROUP BY H.hacker_id, H.name
-HAVING COUNT(H.hacker_id) > 1
-ORDER BY COUNT(H.hacker_id) DESC, H.hacker_id;
+select H.hacker_id, H.name
+from Submissions as S
+ inner join Challenges as C on S.challenge_id = C.challenge_id
+ inner join Difficulty as D on C.difficulty_level = D.difficulty_level
+ inner join Hackers as H on S.hacker_id = H.hacker_id
+where S.score = D.score and C.difficulty_level = D.difficulty_level
+group by H.hacker_id, H.name
+having count (H.hacker_id) > 1
+order by count(H.hacker_id) desc , H.hacker_id;
